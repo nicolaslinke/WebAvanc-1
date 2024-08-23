4305a430ccbc9cdc077e2842b65cca4c749fc95d
n <2185802@cegepmontpetit.ca>
2024-08-23 16:56:11 -0400

    FCT Import

---
+++ MVCEtWebAPI/Models/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCEtWebAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ MVCEtWebAPI/WebAPI/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public ActionResult PublicTest()
+        {
+            return Ok(new string[] { "Pomme", "Poire", "Banane" });
+        }
+
+        [HttpGet]
+        [Authorize]
+        public ActionResult PrivateTest()
+        {
+            return Ok(new string[] { "PrivatePomme", "PrivatePoire", "PrivateBanane" });
+        }
+    }
+}
