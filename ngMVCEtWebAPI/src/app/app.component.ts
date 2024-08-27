f007183d0acb293ab41132d3815d13cad2593509
n <2185802@cegepmontpetit.ca>
2024-08-27 17:32:28 -0400

    FCT Continuation rencontre 2

--- ngMVCEtWebAPI/src/app/app.component.ts
+++ ngMVCEtWebAPI/src/app/app.component.ts
@@ -8,14 +8,32 @@ import { lastValueFrom } from 'rxjs';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+  username ?: string;
+
   title = 'ngMVCEtWebAPI';
 
   constructor(public http : HttpClient){}
 
   async testPublic() : Promise<void>{
-    console.log("working");
     let x = await lastValueFrom(this.http.get("https://localhost:7269/api/Account/PublicTest"));
     console.log(x);
+  }
+
+  async testPrivate() : Promise<void>{
+    let x = await lastValueFrom(this.http.get("https://localhost:7269/api/Account/PrivateTest"));
+    console.log(x);
+  }
+
+  async enregistrer() : Promise<void>{
+    let x = await lastValueFrom(this.http.post("https://localhost:7269/api/Account/Register", this.username));
+    console.log(x);
+  }
+
+  async login() : Promise<void>{
+    let x = await lastValueFrom(this.http.post("https://localhost:7269/api/Account/Login", this.username));
+    console.log(x);
+    
   }
 }
 
